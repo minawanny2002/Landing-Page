ced8b8de33db8a98848768f6a2095fc0097c025a
minawanny2002 <mina_wanny@hotmail.com>
2024-07-09 04:05:22 +0300

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+// const firstNav = $('#firstnav');
+// const secondNav = $('#secondnav');
+// const home = $('.home');
+// const firstSec = $('.first-sec');
+
+// home.outerHeight(firstSec.outerHeight() - (firstNav.outerHeight()+secondNav.outerHeight())); 
