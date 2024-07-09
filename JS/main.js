0350220d1b69b19b6f90123914ca59103a77c7b9
minawanny2002 <mina_wanny@hotmail.com>
2024-07-09 16:38:29 +0300

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,11 @@
-// const firstNav = $('#firstnav');
-// const secondNav = $('#secondnav');
-// const home = $('.home');
-// const firstSec = $('.first-sec');
+const closeBtn1 = document.querySelector('.xmark-1');
+const closeBtn2 = document.querySelector('.xmark-2');
+const label1 = document.querySelector('.inp-1');
+const label2 = document.querySelector('.inp-2');
 
-// home.outerHeight(firstSec.outerHeight() - (firstNav.outerHeight()+secondNav.outerHeight())); 
+closeBtn1.addEventListener('click' , function(){
+    label1.value="";
+})
+closeBtn2.addEventListener('click' , function(){
+    label2.value="";
+})
